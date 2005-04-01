bfde67af831ab736be6b6d394d0e00e75fc9add7
yann <yann@siav>
2005-04-01 17:02:32 +0000

    [project @ svn-v3-undefined:8a708873-67e9-0310-be3a-b2333ca284a8:trunk:146]  * dotemacs.el: do not debug-on-error by default  (diff-buffer-with-associated-file): new util function  (global-set-key): change meaning of C-x k to diff when unsaved

--- dotemacs.el
+++ dotemacs.el
@@ -6,8 +6,6 @@
 ;; Hi-lock: ((";; \\(.*\\)" (1 'italic append)))
 ;; Hi-lock: end
 
-(toggle-debug-on-error)
-
 ;;; Basis
 ;; Load site-specific stuff: paths, accounts, projects...
 (if (file-exists-p (expand-file-name "~/.emacs-local"))
@@ -405,6 +403,79 @@
 ;; I hate to mix different compilations contexts (see make -C.. for example...)
 (make-variable-buffer-local 'compile-history)
 
+(defun diff-buffer-with-associated-file ()
+  "View the differences between BUFFER and its associated file.
+This requires the external program \"diff\" to be in your `exec-path'.
+Returns nil if no differences found, 't otherwise."
+  (interactive)
+  (let ((buf-filename buffer-file-name)
+        (buffer (current-buffer)))
+    (unless buf-filename
+      (error "Buffer %s has no associated file" buffer))
+    (let ((diff-buf (get-buffer-create
+                     (concat "*Assoc file diff: "
+                             (buffer-name)
+                             "*"))))
+      (with-current-buffer diff-buf
+        (setq buffer-read-only nil)
+        (erase-buffer))
+      (let ((tempfile (make-temp-file "buffer-to-file-diff-")))
+        (unwind-protect
+            (progn
+              (with-current-buffer buffer
+                (write-region (point-min) (point-max) tempfile nil 'nomessage))
+              (if (zerop
+                   (apply #'call-process "diff" nil diff-buf nil
+                          (append
+                           (when (and (boundp 'ediff-custom-diff-options)
+                                      (stringp ediff-custom-diff-options))
+                             (list ediff-custom-diff-options))
+                           (list buf-filename tempfile))))
+                  (progn
+                    (message "No differences found")
+                    nil)
+                (progn
+                  (with-current-buffer diff-buf
+                    (goto-char (point-min))
+                    (if (fboundp 'diff-mode)
+                        (diff-mode)
+                      (fundamental-mode)))
+                  (display-buffer diff-buf)
+                  t)))
+          (when (file-exists-p tempfile)
+            (delete-file tempfile)))))))
+
+;; tidy up diffs when closing the file
+(defun kill-associated-diff-buf ()
+  (let ((buf (get-buffer (concat "*Assoc file diff: "
+                             (buffer-name)
+                             "*"))))
+    (when (bufferp buf)
+      (kill-buffer buf))))
+
+(add-hook 'kill-buffer-hook 'kill-associated-diff-buf)
+
+(defun de-context-kill (arg)
+  "Kill buffer, taking gnuclient into account."
+  (interactive "p")
+  (when (and (buffer-modified-p)
+             buffer-file-name
+             (not (string-match "\\*.*\\*" (buffer-name)))
+             ;; erc buffers will be automatically saved
+             (not (eq major-mode 'erc-mode))
+             (= 1 arg))
+    (let ((differences 't))
+    (when (file-exists-p buffer-file-name)
+      (setq differences (diff-buffer-with-associated-file)))
+    (error (if differences
+               "Buffer has unsaved changes"
+             "Buffer has unsaved changes, but no differences wrt. the file"))))
+  (if (and (boundp 'gnuserv-minor-mode)
+             gnuserv-minor-mode)
+      (gnuserv-edit)
+    (set-buffer-modified-p nil)
+    (kill-buffer (current-buffer))))
+
 
 ;;; Global key bindings
 
@@ -445,10 +516,10 @@
 
 (global-set-key (kbd "C-x C-c") 'exit-no-gnuserv-or-frame)
 (global-set-key (kbd "C-x 5 3") 'make-main-frame)
-(global-set-key (kbd "C-x k") (lambda (arg) (interactive "P")
-                                (if (null arg)
-                                    (kill-this-buffer)
-                                  (mapcar (lambda (buf) (kill-buffer buf)) (buffer-list)))))
+(global-set-key (kbd "C-c d") 'diff-buffer-with-associated-file)
+(global-set-key (kbd "C-x k") 'de-context-kill)
+(global-set-key (kbd "C-x K") (lambda () (interactive) (dolist (buf (buffer-list)) (kill-buffer buf))))
+
 (global-set-key (kbd "C-c m") (lambda () (interactive) (gnus 2)))
 (global-set-key (kbd "C-c x") 'chmod-file)
 (global-set-key (kbd "C-c i") 'init)
