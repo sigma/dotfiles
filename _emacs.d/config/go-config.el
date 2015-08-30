e4a88f0618107743f454e77f420d776a3879989e
Yann Hodique <hodiquey@vmware.com>
2015-08-30 08:01:52 -0700

    add go config

---
+++ _emacs.d/config/go-config.el
@@ -0,0 +1,60 @@
+;;; go-config.el --- Configuration for go language
+
+;; Copyright (C) 2015  Yann Hodique
+
+;; Author: Yann Hodique <hodiquey@vmware.com>
+;; Keywords: languages
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'go-flymake)
+
+(defun yh/go-mode-hook ()
+  ;; Use goimports instead of go-fmt
+  (setq gofmt-command "goimports")
+  ;; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save t)
+
+  ;; Customize compile command to run go build
+  (if (not (and (stringp compile-command)
+                (string-match "go" compile-command)))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet && golint"))
+
+  ;; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump)
+
+  ;; company-go
+  (set (make-local-variable 'company-backends) '(company-go))
+  (company-mode)
+
+  ;; go-eldoc
+  (go-eldoc-setup)
+
+  (subword-mode 1)
+  (glasses-mode 1))
+
+(eval-after-load 'go-mode
+  '(add-hook 'go-mode-hook 'yh/go-mode-hook))
+
+(provide 'go-config)
+;;; go-config.el ends here
